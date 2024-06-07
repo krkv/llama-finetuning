8111ed650310ee30abb5370a2f49baf35f5e9ccd
Rodion Kryuchkov <rodion.krjutskov@gmail.com>
2024-06-07 18:56:12 +0300

    Update training scripts

--- finetune_llama_2_7b_chat_hf.sh
+++ finetune_llama_2_7b_chat_hf.sh
@@ -8,8 +8,8 @@
 #SBATCH --time=07:00:00
 
 module load any/python/3.9.9
-source ~/ttm/llama/venv_llama/bin/activate
+source ~/ttm/llama-finetuning/venv-llama-finetuning/bin/activate
 
-srun python ~/ttm/llama-recipes/recipes/finetuning/finetuning.py --dataset "custom_dataset" --custom_dataset.file ~/ttm/llama-dataset/energy_dataset.py --use_peft --peft_method lora --quantization --model_name meta-llama/Llama-2-7b-chat-hf --output_dir ~/ttm/llama/finetuned/llama-2-7b-chat-hf/03 --use_wandb
+srun python ~/ttm/llama-recipes/recipes/finetuning/finetuning.py --dataset "custom_dataset" --custom_dataset.file ~/ttm/llama-dataset/energy_dataset.py --use_peft --peft_method lora --quantization --model_name meta-llama/Llama-2-7b-chat-hf --output_dir ~/ttm/llama-finetuning/finetuned/llama-2-7b-chat-hf/03 --use_wandb
 
 
